15d05e9c2c62f7490f5937901e75e47aa2e521be
Zin KO <zinko.it@outlook.com>
2024-08-30 03:54:05 +0630

    Added equality check function

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -1,1 +1,10 @@
 // TODO: Write Your JavaScript Code Here
+function checkEquality(value1, value2) {
+  if (value1 === value2) {
+    console.log("They are equal in type and value");
+  } else if (value1 == value2) {
+    console.log("They are equal in value");
+  } else {
+    console.log("The values are not equal");
+  }
+}
